cedaf1d20b7caeeb3a86af337c5fd237e8b4db4c
thomasrz <thomaszwetsloot@icloud.com>
2025-09-25 12:39:06 -0400

    added more to example

--- csce242/examples/js/js/loops.js
+++ csce242/examples/js/js/loops.js
@@ -12,6 +12,8 @@ document.getElementById("btn-display").onclick = () => {
 }
 
 document.getElementById("btn-count").onclick = () => {
+    const countDiv = document.getElementById("div-count");
+    const ul = document.createElement("ul");
     const error = document.getElementById("error-number");
     error.innerHTML = "";
 
@@ -26,5 +28,47 @@ document.getElementById("btn-count").onclick = () => {
         return;
     }
 
+    for (let i = start; i <= end; ++i) {
+        const li = document.createElement("li");
+        li.innerHTML = i;
+        console.log(li.innerHTML);
+        ul.append(li);
+    }
 
+    countDiv.append(ul);
+}
+
+document.getElementById("btn-toys").onclick = () => {
+    const toyList = document.getElementById("toy-list");
+    toyList.innerHTML = "";
+
+    const toys = ["ball", "frisbee", "rc car", "the horrors", "doll"];
+
+    for (const toy in toys) {
+        const li = document.createElement("li");
+        li.innerHTML = toy;
+        toyList.append(li);
+    }
+}
+
+document.getElementById("btn-toy-prices").onclick = () => {
+    const toys = [];
+    toys["ball"] = "0.48";
+    toys["frisbee"] = "1.30";
+    toys["rc car"] = "10.99";
+    toys["the horrors"] = "0.00";
+    toys["doll"] = "4.90";
+
+    const toyDiv = document.getElementById("div-toy-prices");
+    toyDiv.innerHTML = "";
+
+    for (const toy in toys) {
+        const p = document.createElement("p");
+        p.innerHTML = toy;
+        toyDiv.append(p);
+
+        p.onclick = () => {
+            document.getElementById("info").innerHTML = `${toy} costs $${toys[toy]}.`
+        }
+    }
 }
